87fc3066b9267f5755952fdb6f20cebb175c66a5
Olof <olof.enstrom@gmail.com>
2022-07-06 18:16:51 +0200

    Show contacts after add, fix missing parameter from dialog

--- __init__.py
+++ __init__.py
@@ -16,61 +16,60 @@
 from mycroft import MycroftSkill, intent_handler
 from mycroft.messagebus import Message
 import sqlite3
-import requests
 
 class Contacts(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
-        con = self.get_con("rwc")
-        con.executescript("CREATE TABLE IF NOT EXISTS contacts(`name` TEXT NOT NULL, `email` TEXT NOT NULL UNIQUE, `phone` TEXT NOT NULL UNIQUE)")
-        self.commit(con)
+        self.con = self.get_con("rwc")
+        self.con.executescript("CREATE TABLE IF NOT EXISTS contacts(`name` TEXT NOT NULL, `email` TEXT NOT NULL UNIQUE, `phone` TEXT NOT NULL UNIQUE)")
+        self.con.commit()
+        self.con.close()
     
     def get_con(self, mode="rw"):
         return sqlite3.connect(f"file:contacts.db?mode={mode}", uri=True)
-    
-    def commit(self, con):
-        con.commit()
-        con.close()
-
 
     @intent_handler("AddContact.intent")
     def add_contact(self, message):
         """Ask for name, email and phone number. Then try to insert that contact into the database."""
         name  = self.get_response("AskForName")
-        self.speak(name)
+        # self.speak(name)
         email = self.get_response("AskForEmail")
-        email = email.replace("punkt", ".").replace("snabel-a", "@").replace(" ", "")
-        self.speak(email)
+        email = email.replace("punkt", ".").replace("snabel-a", "@").replace("snabela", "@").replace(" ", "")
+        # self.speak(email)
         phone = self.get_response("AskForPhone")
         phone = phone.replace("-", "").replace(" ", "")
-        self.speak(phone)
+        # self.speak(phone)
 
         if not name or not email or not phone:
             self.speak_dialog("CouldNotAdd")
             return
 
         try:
-            con = self.get_con()
-            con.execute("INSERT INTO contacts VALUES(?, ?, ?)", (name, email, phone))
+            self.con = self.get_con()
+            self.con.execute("INSERT INTO contacts VALUES(?, ?, ?)", (name, email, phone))
+            self.__display_contacts(self.__get_contacts(self.con))
+            self.con.commit()
             self.speak_dialog("ContactAdded", data={"name": name, "email": email, "phone": phone})
-            self.commit(con)
         except sqlite3.IntegrityError as err:
             self.speak_dialog("NotUnique")
-        except Exception:
+        except sqlite3.DatabaseError as e:
             self.speak_dialog("Error")
+        finally:
+            self.con.close()
 
 
     @intent_handler("ListContacts.intent")
     def list_contacts(self, message):
         """List all contacts"""
         try:
-            con = self.get_con()
-            contact_list = con.execute("SELECT * FROM contacts ORDER BY name ASC").fetchall()
-            self.__display_contacts(contact_list)
+            self.con = self.get_con()
+            self.__display_contacts(self.__get_contacts(self.con))
             self.speak_dialog("ShowContacts")
         except Exception as e:
             self.log.info(e)
             self.speak_dialog("Error")
+        finally:
+            self.con.close()
 
 
     @intent_handler("RemoveContact.intent")
@@ -80,10 +79,12 @@ class Contacts(MycroftSkill):
         response = self.get_response("Who")
         if response:
             try:
-                con = self.get_con()
-                contact_list = con.execute("SELECT * FROM contacts WHERE name=?", (response,)).fetchall()
+                self.con = self.get_con()
+                contact_list = self.con.execute("SELECT * FROM contacts WHERE name=?", (response,)).fetchall()
             except Exception:
                 self.speak_dialog("Error")
+            finally:
+                self.con.close()
 
             if len(contact_list) <= 0:
                 self.speak_dialog("NotFound")
@@ -96,6 +97,10 @@ class Contacts(MycroftSkill):
             
             self.__delete_contact(response)
     
+    def __get_contacts(self, con):
+        """Get contacts from the database."""
+        return con.execute("SELECT * FROM contacts ORDER BY name ASC").fetchall()
+
     def __delete_contact(self, contact, con):
         """Try to delete the contact from the database."""
         try:
@@ -104,6 +109,8 @@ class Contacts(MycroftSkill):
             self.speak_dialog("ContactRemoved", data={"name": contact.name, "phone": contact.phone})
         except Exception:
             self.speak_dialog("Error")
+        finally:
+            con.close()
     
 
     def __display_contacts(self, contacts):
